3e669eccf52b4e244787e1ed10aa9d85770268c5
Gabriel Saraiva <extremez3r0@gmail.com>
2019-06-26 10:26:32 +0000

    Remove rails files

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #
-
-  get '/test', to: 'application#index'
-  get '/xoxo', to: 'application#xoxo'
-
-end
